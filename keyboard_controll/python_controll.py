ab3dc164ee67fcecba0079f21bc497a0801c43e8
Junsoo Park <junsoo.feb@gmail.com>
2020-01-14 21:54:43 +0900

    initial commit

---
+++ keyboard_controll/python_controll.py
@@ -0,0 +1,47 @@
+import cv2
+import serial
+
+arduino = serial.Serial('/dev/ttyUSB0', 9600)
+
+cap = cv2.VideoCapture(-1)  
+while(True):  
+    ret, frame = cap.read() 
+    cv2.imshow('frame',frame)
+    key = cv2.waitKey(1) & 0xFF	    
+    if key == ord('q'):  
+        break
+
+    elif key ==ord('w'):          
+        arduino.write(b'w')
+        print('w')
+
+    elif key ==ord('a'):         
+        arduino.write(b'a')
+        print('a')
+
+    elif key ==ord('d'):          
+        arduino.write(b'd')
+        print('d')
+
+    elif key ==ord('s'):          
+        arduino.write(b's')
+        print('s')
+
+    elif key ==ord('r'):          
+        arduino.write(b'r')
+        print('r')
+
+    elif key ==ord('f'):          
+        arduino.write(b'f')
+        print('f')
+
+    elif key ==ord('g'):          
+        arduino.write(b'g')
+        print('g')
+
+    elif key ==ord('h'):          
+        arduino.write(b'h')
+        print('h')
+
+cap.release()  
+cv2.destroyAllWindows()  
