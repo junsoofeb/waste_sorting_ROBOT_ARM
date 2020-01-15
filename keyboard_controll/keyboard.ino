fe3205a0feee105905363c3c36c4adefbab0868b
Junsoo Park <junsoo.feb@gmail.com>
2020-01-16 00:09:11 +0900

    bug fix

--- keyboard_controll/keyboard.ino
+++ keyboard_controll/keyboard.ino
@@ -12,6 +12,8 @@ int body;
 int left_arm; 
 int right_arm;
 int claw; 
+
+int move = 10;
     
 
 void setup() {
@@ -20,26 +22,26 @@ void setup() {
   PIN[0] = 9; //서보모터 IO를 9번핀으로 지정
   MIN[0] = 0; //서보모터 최소 회전각도
   MAX[0] = 180; //서보모터 최대 회전각도
-  INITANGLE[0] = 0; //서보모터 초기각도
+  INITANGLE[0] = 100; //서보모터 초기각도
   
   
   //왼쪽 서보모터
   PIN[1] = 6;
   MIN[1] = 0; 
   MAX[1] = 180;
-  INITANGLE[1] = 0;
+  INITANGLE[1] = 40;
 
   //오른쪽 서보모터
   PIN[2] = 5;
   MIN[2] = 0;
   MAX[2] = 180;
-  INITANGLE[2] = 0;
+  INITANGLE[2] = 40;
 
   //집게  서보모터
   PIN[3] = 10;
   MIN[3] = 0;
-  MAX[3] = 180;
-  INITANGLE[3] = 0;
+  MAX[3] = 70;
+  INITANGLE[3] = 60;
 
   // 처음 위치 잡기
   for (int i = 0; i < SERVOS; i++)
@@ -49,10 +51,10 @@ void setup() {
     currentAngle[i]=INITANGLE[i];
   }
 
-  int body = INITANGLE[0];
-  int left_arm = INITANGLE[1];
-  int right_arm = INITANGLE[2];
-  int claw = INITANGLE[3];
+  body = INITANGLE[0];
+  left_arm = INITANGLE[1];
+  right_arm = INITANGLE[2];
+  claw = INITANGLE[3];
     
 
   //mysevo[0].attach(9); // 좌우회전 모터
@@ -80,16 +82,19 @@ void loop() {
     {
       // w 누르면 로봇팔 앞으로 
       case 'w':
-        right_arm += 5;
+        right_arm += move;
+        left_arm -= move;
+        if(left_arm <= 0) 
+          left_arm = 0;   
         if(right_arm >= 180) 
           right_arm = 180;
+        myservo[1].write(left_arm);
         myservo[2].write(right_arm); // 오른쪽팔 모터
-        //delay(1000);
         break;
         
       // a 누르면 몸통 왼쪽으로
       case 'a':
-        body += 5;
+        body += move;
         if(body >= 180) 
           body = 180;
         myservo[0].write(body);
@@ -98,7 +103,7 @@ void loop() {
         
       // d 누르면 몸통 오른쪽으로
       case 'd':
-        body -= 5;
+        body -= move;
         if (body <= 0)
           body = 0;
         myservo[0].write(body);
@@ -107,44 +112,25 @@ void loop() {
         
       // s 누르면 로봇팔 뒤로 
       case 's':
-        right_arm -= 5;
-        if(right_arm <= 0) 
-          right_arm = 0;
-        myservo[2].write(right_arm); // 오른쪽팔 모터
-        //delay(1000);
-        break;
-        
-      // f 누르면 팔 아래로
-      case 'f':
-        left_arm += 5;
+        right_arm -= move;
+        left_arm += move;
         if(left_arm >= 180) 
           left_arm = 180;
+        if(right_arm <= 0) 
+          right_arm = 0;
         myservo[1].write(left_arm);
-        //delay(1000);
-        break;
-        
-      // r 누르면 로봇팔 위로  
-      case 'r':
-        left_arm -= 5;
-        if(left_arm <= 0) 
-          left_arm = 0;
-        myservo[1].write(left_arm);
+        myservo[2].write(right_arm); // 오른쪽팔 모터
         //delay(1000);
         break;
         
       // g 누르면 집게 잡기
       case 'g':
-        claw += 5;
-        if(claw >= 180)
-          claw = 180;
+        claw = 0;
         myservo[3].write(claw); // 집게 모터
-        //delay(1000);
         break;
         
       case 'h':
-        claw -= 5;
-        if(claw <= 0)
-           claw = 0;
+        claw = 60;
         myservo[3].write(claw); // 집게 모터
         //delay(1000);
         break;
